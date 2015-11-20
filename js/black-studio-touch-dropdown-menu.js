2a873e0cdf1a1bb0cbcd7a05b2dd0eff50c0f2a7
Marco Chiesi <marco@blackstudio.it>
2015-11-20 14:27:36 +0100

    Minor js adjustments

--- js/black-studio-touch-dropdown-menu.js
+++ js/black-studio-touch-dropdown-menu.js
@@ -5,15 +5,18 @@
 ( function( $ ) {
 
 	/* Detect device in use  */
-	var black_studio_is_touch = ( ( 'ontouchstart' in window ) || ( window.navigator.msPointerEnabled ) );
-	var black_studio_is_ios5 = /iPad|iPod|iPhone/.test( navigator.platform ) && 'matchMedia' in window;
-	var black_studio_touch_dropdown_menu_apply = black_studio_is_touch && ! black_studio_is_ios5;
-	var black_studio_superfish_fix = false;
+	var black_studio_is_touch = ( ( 'ontouchstart' in window ) || ( window.navigator.msPointerEnabled ) ),
+		black_studio_is_ios5 = /iPad|iPod|iPhone/.test( navigator.platform ) && 'matchMedia' in window,
+		black_studio_touch_dropdown_menu_apply = black_studio_is_touch && ! black_studio_is_ios5,
+		black_studio_superfish_fix = false;
 
 	/* Apply dropdown effect on first click */
 	if ( black_studio_touch_dropdown_menu_apply ) {
+
 		$( document ).ready(function() {
+
 			$( black_studio_touch_dropdown_menu_params.selector ).each(function() {
+
 				var $this = $(this);
 
 				// Fix for IE
@@ -24,21 +27,24 @@
 				
 				// Touch Handler
 				$this.bind( 'touchstart', function() {
-					
+					var i, noclick;
+
 					// Hack for superfish menus with low delay
 					if ( ! black_studio_superfish_fix && $.fn.superfish !== undefined ) {
-						for ( var i = 0; i < $.fn.superfish.o.length; i++ ) {
+						for ( i = 0; i < $.fn.superfish.o.length; i++ ) {
 							$.fn.superfish.o[i].delay = 800;
 						}
 						black_studio_superfish_fix = true;
 					}
-					
-					var noclick = ! $this.data( 'dataNoclick' );
+
+					// Reset other menus
+					noclick = ! $this.data( 'dataNoclick' );
 					$( black_studio_touch_dropdown_menu_params.selector ).each(function() {
 						$( this ).data( 'dataNoclick', false);
-					}); 
+					});
 					$this.data( 'dataNoclick', noclick );
 					this.focus();
+
 				}); // end touchstart
 
 				// Click Handler
@@ -48,10 +54,11 @@
 					}
 					this.focus();
 				}); // end click
+
 			}); // end each
 
 			// Fix for 3rd+ level menus not working in some circumstances
-			$( black_studio_touch_dropdown_menu_params.selector_leaf ).each(function() { 
+			$( black_studio_touch_dropdown_menu_params.selector_leaf ).each(function() {
 				$( this ).bind( 'touchstart', function(){
 					window.location = this.href;
 				}); // end touchstart
